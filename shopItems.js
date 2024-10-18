f987a53b717762ec74e765766455019666a9a5f2
Sachini Peduruhewa <sachinipeduruhewa@gmail.com>
2024-10-18 15:11:06 +0530

    shop updated

---
+++ shopItems.js
@@ -0,0 +1,160 @@
+export const products = [
+    {
+        "id": "1",
+        "productName": "Tea Fertilizer",
+        "detail1": "Mixture: TU-709",
+        "detail2": "Weight: 10kg",
+        "productPrice": "1000",
+        "productImg": "images/Shop/1.png"
+    },
+
+    {
+        "id": "2",
+        "productName": "Tea Fertilizer",
+        "detail1": "Mixture: T-709",
+        "detail2": "Weight: 10kg",
+        "productPrice": "1200",
+        "productImg": "images/Shop/1.png"
+    },
+
+    {
+        "id": "3",
+        "productName": "Coconut Fertilizer",
+        "detail1": "Zone: Wet Zone",
+        "detail2": "Weight: 5kg",
+        "productPrice": "500",
+        "productImg": "images/Shop/3.png"
+    },
+
+    {
+        "id": "4",
+        "productName": "Coconut Fertilizer",
+        "detail1": "Zone: Dry Zone",
+        "detail2": "Weight: 5kg",
+        "productPrice": "500",
+        "productImg": "images/Shop/3.png"
+    },
+
+    {
+        "id": "5",
+        "productName": "Paddy Fertilizer",
+        "detail1": "Stage: Basal",
+        "detail2": "Weight: 15kg",
+        "productPrice": "2500",
+        "productImg": "images/Shop/5.png"
+    },
+
+    {
+        "id": "6",
+        "productName": "Paddy Fertilizer",
+        "detail1": "Stage: TDM",
+        "detail2": "Weight: 15kg",
+        "productPrice": "3000",
+        "productImg": "images/Shop/5.png"
+    },
+
+    {
+        "id": "7",
+        "productName": "Cinnamon Fertilizer",
+        "detail1": "Type: Cinnamon-U",
+        "detail2": "Weight: 5kg",
+        "productPrice": "700",
+        "productImg": "images/Shop/7.png"
+    },
+
+    {
+        "id": "8",
+        "productName": "Vegetable Fertilizer",
+        "detail1": "Stage: Basal",
+        "detail2": "Weight: 20kg",
+        "productPrice": "8000",
+        "productImg": "images/Shop/8.png"
+    },
+
+    {
+        "id": "9",
+        "productName": "Chillie Fertilizer",
+        "detail1": "Type: Chilli-SP",
+        "detail2": "Weight: 2kg",
+        "productPrice": "300",
+        "productImg": "images/Shop/9.png"
+    },
+
+    {
+        "id": "10",
+        "productName": "Rolex watch",
+        "productPrice": "2000",
+        "productImg": "watch.png"
+    },
+
+    {
+        "id": "11",
+        "productName": "Osprey Backpack",
+        "productPrice": "49.98",
+        "productImg": "bag.png"
+    },
+
+    {
+        "id": "12",
+        "productName": "hidesign Mens Footwear",
+        "productPrice": "69.86",
+        "productImg": "black-shoes.png"
+    },
+
+    {
+        "id": "13",
+        "productName": "Raymond Men's Trousers",
+        "productPrice": "7.82",
+        "productImg": "cargo-shorts.png"
+    },
+
+    {
+        "id": "14",
+        "productName": "Titan Eyeglass",
+        "productPrice": "14.00",
+        "productImg": "cat-eye-glasses.png"
+    },
+
+    {
+        "id": "15",
+        "productName": "Russell Athletic's hoodie",
+        "productPrice": "22.50",
+        "productImg": "hoodie.png"
+    },
+
+    {
+        "id": "16",
+        "productName": "Apple MacBook Pro",
+        "productPrice": "1599.00",
+        "productImg": "laptop.png"
+    },
+
+    {
+        "id": "17",
+        "productName": "Levi's shirt",
+        "productPrice": "7.04",
+        "productImg": "shirt.png"
+    },
+
+    {
+        "id": "18",
+        "productName": "rock dove slippers",
+        "productPrice": "28.00",
+        "productImg": "slipper.png"
+    },
+
+    {
+        "id": "19",
+        "productName": "t-shirts at J.Crew",
+        "productPrice": "19.50",
+        "productImg": "t-shirt.png"
+    },
+
+    {
+        "id": "20",
+        "productName": "Rolex watch",
+        "productPrice": "2000",
+        "productImg": "watch.png"
+    }
+
+]   
